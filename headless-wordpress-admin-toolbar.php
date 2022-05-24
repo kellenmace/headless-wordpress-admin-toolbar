e9a72acb7e9d66b758632fd7aa9f114cca429961
Jordan Maslyn <jordan@maslyn.us>
2022-05-24 15:05:26 -0400

    Find and replace Faust FE URL with WP URL

--- headless-wordpress-admin-toolbar.php
+++ headless-wordpress-admin-toolbar.php
@@ -13,10 +13,12 @@
 
 require_once ABSPATH . WPINC . '/class-wp-admin-bar.php';
 
+$admin_bar_menu_item_type = 'AdminBarMenuItem';
+$admin_bar_menu_item_meta_type = 'AdminBarMenuItemMeta';
+$admin_bar_menu_items_field_name = 'adminBarMenuItems';
+
 add_action('graphql_register_types', function () {
-    $admin_bar_menu_item_type = 'AdminBarMenuItem';
-    $admin_bar_menu_item_meta_type = 'AdminBarMenuItemMeta';
-    $field_name = 'adminBarMenuItems';
+    global $admin_bar_menu_item_type, $admin_bar_menu_item_meta_type, $admin_bar_menu_items_field_name;
 
     register_graphql_object_type($admin_bar_menu_item_meta_type, [
         'description' => __('A single node from the admin bar menu', 'faust-admin-bar'),
@@ -62,27 +64,27 @@ add_action('graphql_register_types', function () {
         ],
     ]);
 
-    register_graphql_field('ContentNode', $field_name, [
+    register_graphql_field('ContentNode', $admin_bar_menu_items_field_name, [
         'type' => ['list_of' => $admin_bar_menu_item_type],
         'resolve' => 'resolve_admin_bar_menu_nodes',
     ]);
 
-    register_graphql_field('TermNode', $field_name, [
+    register_graphql_field('TermNode', $admin_bar_menu_items_field_name, [
         'type' => ['list_of' => $admin_bar_menu_item_type],
         'resolve' => 'resolve_admin_bar_menu_nodes',
     ]);
 
-    register_graphql_field('User', $field_name, [
+    register_graphql_field('User', $admin_bar_menu_items_field_name, [
         'type' => ['list_of' => $admin_bar_menu_item_type],
         'resolve' => 'resolve_admin_bar_menu_nodes',
     ]);
 
-    register_graphql_field('RootQueryToMediaItemConnection', $field_name, [
+    register_graphql_field('RootQueryToMediaItemConnection', $admin_bar_menu_items_field_name, [
         'type' => ['list_of' => $admin_bar_menu_item_type],
         'resolve' => 'resolve_admin_bar_menu_nodes',
     ]);
 
-    register_graphql_field('RootQueryToUserConnection', $field_name, [
+    register_graphql_field('RootQueryToUserConnection', $admin_bar_menu_items_field_name, [
         'type' => ['list_of' => $admin_bar_menu_item_type],
         'resolve' => 'resolve_admin_bar_menu_nodes',
     ]);
@@ -92,7 +94,7 @@ add_action('graphql_register_types', function () {
     foreach ($graphql_post_types as $graphql_post_type) {
         $single_name = ucfirst($graphql_post_type->graphql_single_name);
 
-        register_graphql_field("RootQueryTo{$single_name}Connection", $field_name, [
+        register_graphql_field("RootQueryTo{$single_name}Connection", $admin_bar_menu_items_field_name, [
             'type' => ['list_of' => $admin_bar_menu_item_type],
             'resolve' => 'resolve_admin_bar_menu_nodes',
         ]);
@@ -106,6 +108,8 @@ function resolve_admin_bar_menu_nodes($source)
     if (!is_user_logged_in() || !_get_admin_bar_pref()) {
         return null;
     }
+
+    add_filter('graphql_request_results', 'headless_wordpress_admin_bar_url_replacement', 11);
 
     $nodes = [];
     $admin_bar_class = apply_filters('wp_admin_bar_class', 'WP_Admin_Bar');
@@ -136,4 +140,41 @@ function resolve_admin_bar_menu_nodes($source)
     }, $nodes);
 
     return $value;
+}
+
+function headless_wordpress_admin_bar_url_replacement($response)
+{
+    if (!function_exists('WPE\FaustWP\Replacement\domain_replacement_enabled') || !WPE\FaustWP\Replacement\domain_replacement_enabled()) {
+        return $response;
+    }
+
+    if (
+        is_object($response) &&
+        property_exists($response, 'data') &&
+        is_array($response->data)
+    ) {
+        headless_wordpress_admin_bar_url_replace_recursive($response->data);
+    }
+
+    return $response;
+}
+
+function headless_wordpress_admin_bar_url_replace_recursive(&$data)
+{
+    global $admin_bar_menu_items_field_name;
+
+    foreach ($data as $key => &$value) {
+        if ($key === $admin_bar_menu_items_field_name) {
+            foreach ($value as &$item) {
+                foreach ($item as $item_key => &$item_value) {
+                    if (('href' === $item_key) && is_string($item_value)) {
+                        $replacement = site_url();
+                        $item_value  = str_replace(WPE\FaustWP\Settings\faustwp_get_setting('frontend_uri', '/'), $replacement, $item_value);
+                    }
+                }
+            }
+        } elseif (is_array($value)) {
+            headless_wordpress_admin_bar_url_replace_recursive($value);
+        }
+    }
 }
